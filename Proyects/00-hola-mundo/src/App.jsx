325bc1b2bd877f2475c25868966682dd5f787e5e
vicente443 <al066219@uacam.mx>
2024-10-24 12:46:47 -0600

    componente twiter

---
+++ Proyects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt= "El avatar de midudev" src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
+                <div>
+                    <strong>Vicente Gonzalez Chable</strong>
+                    <span>@Chente443</span>
+                </div>
+            
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
